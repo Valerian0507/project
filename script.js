5796f1005fcb68ada8e52499fd91008389de9862
Alex <alex.shaginyan0507@gmail.com>
2024-02-27 23:37:00 +0100

    js change

--- script.js
+++ script.js
@@ -21,6 +21,8 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
 
 const personalMovieDB = {
@@ -32,10 +34,27 @@ const personalMovieDB = {
 };
 
 
-for(let i =0; i < 2; i++){
+for(let i =0; i < 2; i++) {
     const   a = prompt('Один из последних просмотренных фильмов?', ''),
             b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length <50) {
         personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+    }
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10  && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка")
 }
 
 console.log(personalMovieDB);
